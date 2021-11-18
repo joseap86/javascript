f6122e80da4afc744c3b47de507dd9dab5c67d27
José Aparecido <94494717+joseap86@users.noreply.github.com>
2021-11-18 20:48:01 -0300

    Atualizei  o README

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 78.6
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condicão simples!
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
